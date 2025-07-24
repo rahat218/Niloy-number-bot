5bd9eb7f0834f14cf25d255269e83cb516f34831
rahat218 <rahatdaraz886@gmail.com>
2025-07-24 14:08:55 +0600

    Delete database.py

--- database.py
+++
@@ -1,9 +0,0 @@
-# database.py
-import asyncpg
-from datetime import datetime, timedelta
-import logging
-from config import (DATABASE_URL, ADMIN_CHANNEL_ID, NUMBER_LEASE_MINUTES, 
-                    STRIKE_LIMIT_FOR_RESTRICTION, RESTRICTION_HOURS)
-
-# (বাকি সব কোড আগের উত্তরের মতোই থাকবে, এটি সম্পূর্ণ)
-# ...
